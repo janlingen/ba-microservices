a7a2b28ee9c4db06eae8c23093aa28003b159d68
janlingen <82512348+janlingen@users.noreply.github.com>
2023-08-17 08:59:46 +0200

    Add bezahl-service

---
+++ bezahl-service/src/main/java/de/janlingen/bezahlservice/BezahlServiceApplication.java
@@ -0,0 +1,13 @@
+package de.janlingen.bezahlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezahlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezahlServiceApplication.class, args);
+	}
+
+}

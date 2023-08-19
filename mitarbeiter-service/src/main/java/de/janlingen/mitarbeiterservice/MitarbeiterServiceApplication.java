09c5b7e8d89a5145c49f3d7730932ab7ed37adb4
janlingen <82512348+janlingen@users.noreply.github.com>
2023-08-19 22:04:28 +0200

    Add mitarbeiter-service

---
+++ mitarbeiter-service/src/main/java/de/janlingen/mitarbeiterservice/MitarbeiterServiceApplication.java
@@ -0,0 +1,13 @@
+package de.janlingen.mitarbeiterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitarbeiterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitarbeiterServiceApplication.class, args);
+	}
+
+}

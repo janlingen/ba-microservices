85ddbf676754ad6007931e4f6a4d1c56ffccb47c
janlingen <82512348+janlingen@users.noreply.github.com>
2023-09-25 11:40:36 +0200

    Add bestell-service to ecom-asynchronous

---
+++ ecom-asynchronous/bestell-service/src/test/java/de/janlingen/bestellservice/BestellServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.janlingen.bestellservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestellServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6a4e829353036fcc254354df1ff17879900880c5
janlingen <82512348+janlingen@users.noreply.github.com>
2023-10-13 15:33:24 +0200

    Autocreate a Mitarbeiter to test the application

--- ecom-synchronous/mitarbeiter-service/src/main/java/de/janlingen/mitarbeiterservice/MitarbeiterServiceApplication.java
+++ ecom-synchronous/mitarbeiter-service/src/main/java/de/janlingen/mitarbeiterservice/MitarbeiterServiceApplication.java
@@ -1,7 +1,11 @@
 package de.janlingen.mitarbeiterservice;
 
+import de.janlingen.mitarbeiterservice.data.MitarbeiterDTO;
+import de.janlingen.mitarbeiterservice.service.MitarbeiterService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -12,4 +16,13 @@ public class MitarbeiterServiceApplication {
 		SpringApplication.run(MitarbeiterServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner createInitialMitarbeiter(MitarbeiterService mitarbeiterService) {
+		return args -> {
+			MitarbeiterDTO mitarbeiterDTO = new MitarbeiterDTO();
+			mitarbeiterDTO.setUsername("testMit");
+			mitarbeiterDTO.setPasswort("test");
+			String mitarbeiterId = mitarbeiterService.createMitarbeiter(mitarbeiterDTO);
+		};
+	}
 }
